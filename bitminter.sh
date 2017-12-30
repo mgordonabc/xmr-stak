aca7dc9f7a4899c2416434425c33b67d33a004d0
mgordonabc <mgordonabc@gmail.com>
2017-12-30 21:16:20 +0000

    BITMINTER

---
+++ bitminter.sh
@@ -0,0 +1,11 @@
+sudo apt-add-repository universe
+sudo apt-add-repository multiverse
+
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+
+sudo apt update; sudo apt install oracle-java8-installer
+sudo apt install oracle-java8-set-default
+
+sudo javaws https://bitminter.com/client/bitminter.jnlp
